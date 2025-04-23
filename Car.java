b8d6fdb321ee1fdf59aff95b6ca9370682acca20
RevathiRaviTesting1996 <revathiravitesting1996@gmail.com>
2025-04-23 20:32:47 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,13 @@
+package com.testing.inheritanceExample;
+
+public class Car {
+
+	private int wheels=4;
+	
+	protected void engine() {
+		System.out.println("This is my engine secret");
+		System.out.println(wheels);
+	}
+	
+	
+}
